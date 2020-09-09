a5773e0be8565846b92581ec6d2a6ffee9609f47
Arpita K Sau <arpitakaran@gmail.com>
2020-09-08 23:02:15 -0500

    updated-server.js

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
+//required dependencies
 const express = require("express");
+const morgan = require("morgan")
 const mongoose = require("mongoose");
 
+const PORT = process.env.PORT || 8080
 
-const PORT = process.env.PORT || 8080;
-
+//Initiating express app
 const app = express();
 
+app.use(morgan("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-// routes
-const htmlRoutes = require("./routes/html-routes.js");
-const apiRoutes = require("./routes/api-routes.js");
+// html and API routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
-app.use(htmlRoutes)
-app.use(apiRoutes)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
